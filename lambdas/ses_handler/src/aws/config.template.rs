f77872dbde07b3bf52800849ce49f4973e495e26
Julien de Charentenay, PhD <julien.decharentenay@gmail.com>
2024-07-14 15:01:06 +0100

    Add topic arn function

--- lambdas/ses_handler/src/aws/config.template.rs
+++ lambdas/ses_handler/src/aws/config.template.rs
@@ -2,5 +2,9 @@ pub struct Config {}
 impl Config {
   pub const BUCKET: &'static str  = "${bucket}";
   pub const SES_IDENTITY: &'static str = r"${sesIdentity}";
+
+  pub fn topic_arn() -> Option<String> {
+    std::env::var("SNS_TOPIC_ARN").ok()
+  }
 }
 
